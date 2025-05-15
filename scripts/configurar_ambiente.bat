e6b5e484263bfc77ace1ea027cb333fa822a3564
william-Assis <willian-assis1819@hotmail.com>
2025-05-15 11:09:16 -0500

    Primeiro commit - Estrutura inicial do sistema

---
+++ scripts/configurar_ambiente.bat
@@ -0,0 +1,5 @@
+echo "Verificando Python..."
+python --version
+echo "Instrução para Instalar dependências que está no arquivo requirements.txt" 
+pip install -r requirements.txt 
+echo "Ambiente configurado com sucesso!"
