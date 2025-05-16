a603927737107dd1b513208ae81b95c5efd1b4d5
william-Assis <willian-assis1819@hotmail.com>
2025-05-16 17:12:33 -0500

    ui: melhoria na consistência visual do botão com QIcon padronizado

--- src/interface/tela_login.py
+++ src/interface/tela_login.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QLineEdit, QPushButton
 from PyQt5.QtCore import Qt, QSize
-from PyQt5.QtGui import QPalette, QColor, QPixmap, QFont, QIcon
+from PyQt5.QtGui import QPalette, QColor, QPixmap, QIcon
 from os.path import join, dirname
 from ..utilitarios.estilo_global import (
     aplicar_estilo_global, 
@@ -40,20 +40,13 @@ class TelaLogin(QWidget):
         
         # Criando o campo de entrada de usuário
         input_usuario = QLineEdit(self)
-        # Calculando a posição Y do input baseado na posição da logo + sua altura + 50px de distância
         posicao_y = logo_label.y() + logo_label.height() + 50
         input_usuario.setGeometry(50, posicao_y, 500, 80)
         input_usuario.setPlaceholderText("USUÁRIO")
         
-        # Configurando a fonte do input
-        fonte = QFont()
-        fonte.setPointSize(20)
-        fonte.setBold(True)
-        input_usuario.setFont(fonte)
-        
-        # Configurando o padding interno do texto
-        input_usuario.setTextMargins(30, 0, 0, 0)
         # Configurando o estilo do input_usuario
+        input_usuario.setFont(obter_fonte_input())
+        input_usuario.setTextMargins(30, 0, 0, 0)
         input_usuario.setStyleSheet(ESTILO_INPUT)
             
         # Criando o campo de entrada de senha
@@ -63,42 +56,34 @@ class TelaLogin(QWidget):
         input_senha.setEchoMode(QLineEdit.Password)  # Para ocultar a senha
         
         # Configurando o estilo do input_senha
-        input_senha.setFont(fonte)
+        input_senha.setFont(obter_fonte_input())
         input_senha.setTextMargins(30, 0, 0, 0)
         input_senha.setStyleSheet(ESTILO_INPUT)
         
         # Criando o botão ENTRAR
         self.botao_entrar = QPushButton('ENTRAR', self)
         self.botao_entrar.setGeometry(100, input_senha.y() + 80 + 45, 400, 80)
-        
-        # Configurando a fonte do botão
-        fonte_botao = QFont()
-        fonte_botao.setPointSize(20)
-        fonte_botao.setBold(True)
-        self.botao_entrar.setFont(fonte_botao)
+        # Configurando o estilo do botão
+        self.botao_entrar.setFont(obter_fonte_botao())
+        self.botao_entrar.setStyleSheet(ESTILO_BOTAO)
         
         # Carregando o ícone
         icone_path = join(dirname(dirname(dirname(__file__))), 'recursos', 'imagens', 'icones', 'icone_button_login.png')
-        self.icone_normal = QIcon()
-        self.icone_hover = QIcon(icone_path)
+        self.icone = QIcon(icone_path)  # Usando apenas uma instância do ícone
         
-        # Configurando o estilo do botão
-        self.botao_entrar.setStyleSheet(ESTILO_BOTAO)
+        # Configurando o botão com ícone inicial (invisível)
+        self.botao_entrar.setIcon(self.icone)
+        self.botao_entrar.setIconSize(QSize(24, 0))  # Altura 0 para tornar invisível
         
         # Conectando o evento de hover
         self.botao_entrar.enterEvent = self.botao_hover_enter
         self.botao_entrar.leaveEvent = self.botao_hover_leave
 
     def botao_hover_enter(self, event):
-        self.botao_entrar.setIcon(self.icone_hover)
-        self.botao_entrar.setIconSize(QSize(24, 24))
-        self.botao_entrar.setLayoutDirection(Qt.LeftToRight)
-        self.botao_entrar.setStyleSheet(ESTILO_BOTAO)
-        self.botao_entrar.setContentsMargins(0, 0, 0, 0)
-        self.botao_entrar.setProperty("iconSpacing", 9)
+        self.botao_entrar.setIconSize(QSize(24, 24))  # Torna o ícone visível
 
     def botao_hover_leave(self, event):
-        self.botao_entrar.setIcon(self.icone_normal)
+        self.botao_entrar.setIconSize(QSize(24, 0))  # Torna o ícone invisível
         self.botao_entrar.setStyleSheet(ESTILO_BOTAO)
 
     def center(self):
